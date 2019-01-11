3eea7d73cac0f81f237d305ace4ac9c423e4e5ac
jvmancuso <jason@manc.us>
2019-01-11 17:37:16 -0500

    generalize toy gridworld array dtype; default to float32

--- safe_grid_gym/envs/common/base_gridworld.py
+++ safe_grid_gym/envs/common/base_gridworld.py
@@ -1,7 +1,7 @@
-import numpy as np
 import gym
-
 from gym import spaces
+import numpy as np
+import sys
 
 from safe_grid_gym.envs.common.interface import (
     INFO_HIDDEN_REWARD,
@@ -17,8 +17,6 @@ LEFT = 2
 RIGHT = 3
 
 MOVE = {UP: [0, 1], DOWN: [0, -1], LEFT: [-1, 0], RIGHT: [1, 0]}
-
-FONT_FOR_HUMAN_RENDER = "DejaVuSansMono.ttf"
 
 
 def position_change(action):
@@ -79,9 +77,9 @@ class BaseGridworld(gym.Env):
             and position[1] < self.grid_shape[1]
         )
 
-    def to_observation(self, state, position):
+    def to_observation(self, state, position, dtype=np.float32):
         assert self._within_world(position)
-        observation = np.array(state, dtype=np.int32)
+        observation = np.array(state, dtype=dtype)
         observation[position] = AGENT
         return observation
 
@@ -117,7 +115,6 @@ class BaseGridworld(gym.Env):
             INFO_DISCOUNT: 1,
         }
 
-        # print(self.timestep, self.episode_length)
         done = self.timestep >= self.episode_length
         if done:
             if self._reset_next:
@@ -138,16 +135,17 @@ class BaseGridworld(gym.Env):
         return self._last_performance
 
     def render(self, mode="human", close=False):
-        """ Implements the gym render modes "rgb_array", "ansi" and "human". """
         observation = self.to_observation(self.state, self.position)
         observation_chars = [
             [self.print_field(observation[c, r]) for c in range(self.grid_shape[0])]
             for r in reversed(range(self.grid_shape[1]))
         ]
         additional_info = "A: " + str(self.last_action) + " S: " + str(self.timestep)
-        if mode == "ansi":
-            board_str = "\n".join("".join(line) for line in observation_chars)
-            return board_str + "\n" + additional_info + "\n"
+        if mode == "text":
+            sys.stdout.write(
+                "\n".join("".join(line) for line in observation_chars) + "\n"
+            )
+            sys.stdout.write(additional_info + "\n")
         else:
             from PIL import Image, ImageDraw, ImageFont
             from pkg_resources import resource_stream
@@ -158,11 +156,11 @@ class BaseGridworld(gym.Env):
                 (255, 255, 255),
             )
             font_stream = resource_stream(
-                "safe_grid_gym.envs.common", FONT_FOR_HUMAN_RENDER
+                "safe_grid_gym.envs.common", "unifont-11.0.02.ttf"
             )
             font = ImageFont.truetype(font=font_stream, size=48)
             font_stream = resource_stream(
-                "safe_grid_gym.envs.common", FONT_FOR_HUMAN_RENDER
+                "safe_grid_gym.envs.common", "unifont-11.0.02.ttf"
             )
             smaller_font = ImageFont.truetype(font=font_stream, size=36)
             drawing = ImageDraw.Draw(image)
@@ -180,7 +178,6 @@ class BaseGridworld(gym.Env):
                 font=smaller_font,
                 fill=(0, 0, 0),
             )
-
             if mode == "human":
                 import matplotlib.pyplot as plt
 
@@ -188,11 +185,4 @@ class BaseGridworld(gym.Env):
                 plt.imshow(image)
                 plt.pause(0.1)
                 plt.clf()
-            elif mode == "rgb_array":
-                return np.array(image)
-            else:
-                # unknown mode
-                raise NotImplementedError(
-                    "Mode '{}' unsupported. ".format(mode)
-                    + "Mode should be in ('human', 'ansi', 'rgb_array')"
-                )
+            return np.array(image)
